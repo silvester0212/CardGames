f1194bd938535745ff4fe45eed478df45a5891f1
Silvester Yao <silvester0212@gmail.com>
2018-12-14 17:41:02 -0800

    Build Suit and 100% test

--- src/main/java/Suit.java
+++ src/main/java/Suit.java
@@ -1,3 +1,84 @@
-public enum Suit {
-    SPADE, CLUB, DIAMOND, HEART
+/**
+ * Class Suit
+ * Define the Suit of a Card
+ * @author Silvester Yao
+ */
+public class Suit {
+    /**
+     * Suit Enum
+     * Define all four Suit of a card
+     */
+    public enum SuitEnum {
+        SPADE, CLUB, DIAMOND, HEART
+    }
+
+    private final SuitEnum suit;
+
+    /**
+     * Suit Constructor
+     * @param _suit suit to be created
+     */
+    public Suit (SuitEnum _suit) {
+        suit = _suit;
+    }
+
+    /**
+     * Suit Constructor
+     * @param suitIdx Integer (Suit Enum ordinal) to represent Suit.
+     */
+    public Suit (int suitIdx) {
+        SuitEnum[] allSuits = SuitEnum.values();
+
+        if (suitIdx < 0 || suitIdx >= allSuits.length) {
+            throw new IllegalArgumentException("Suit Index out of bound, must be bewteen 0 to " + (allSuits.length - 1) + ".");
+        }
+
+        this.suit = allSuits[suitIdx];
+    }
+
+    /**
+     * Overrides hashCode method
+     * @return the hashcode of a Suit, implemented using Suit Enum ordinal
+     */
+    public int hashCode() {
+        return this.suit.ordinal();
+    }
+
+    /**
+     * Overrides equals method
+     * @param obj other object to be compared against
+     * @return True if and only if supplied object is of Suit class, and is of the same suit value
+     */
+    public boolean equals(Object obj) {
+        if (obj == null || (! (obj instanceof Suit))) return false;
+        return this.suit == ((Suit) obj).suit;
+    }
+
+    public Suit clone() {
+        return new Suit(this.suit);
+    }
+
+    /**
+     * Ovverrides toString method
+     * @return a string representation of suit
+     */
+    public String toString() {
+        return String.valueOf(this.getSymbol());
+    }
+
+    /**
+     * @return the char symbol for this suit
+     */
+    public char getSymbol() {
+        char r = ' ';
+
+        switch (suit) {
+            case SPADE:     r = '\u2660';   break;
+            case CLUB:      r = '\u2663';   break;
+            case DIAMOND:   r = '\u2666';   break;
+            case HEART:     r = '\u2764';   break;
+        }
+
+        return r;
+    }
 }
---
+++ src/test/java/SuitTest.java
@@ -0,0 +1,129 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuitTest {
+
+    public final static char spadeSymbol = '\u2660';
+    public final static char clubSymbol = '\u2663';
+    public final static char diamondSymbol = '\u2666';
+    public final static char heartSymbol = '\u2764';
+
+    @Test
+    void SpadeTest() {
+        Suit spade = new Suit(Suit.SuitEnum.SPADE);
+
+        // Test Spade HashCode
+        assertEquals(spade.hashCode(), 0);
+
+        // Test Spade Equals
+        assertNotEquals(spade, null);
+        assertNotEquals(spade, 3);
+        assertNotEquals(spade, 'c');
+        assertEquals(spade, new Suit(Suit.SuitEnum.SPADE));
+        assertEquals(spade, new Suit(spade.hashCode()));
+
+        // Test Spade Clone
+        Suit clonedSpade = spade.clone();
+        assertEquals(spade, clonedSpade);
+        assertNotSame(spade, clonedSpade);
+
+        // Test Spade toString
+        assertEquals(spade.toString(), "" + spadeSymbol);
+
+        // Test Spade getSymbol
+        assertEquals(spade.getSymbol(), spadeSymbol);
+
+        System.out.println("Suit Spade tests passed.");
+    }
+
+    @Test
+    void ClubTest() {
+        Suit club = new Suit(Suit.SuitEnum.CLUB);
+
+        // Test Club HashCode
+        assertEquals(club.hashCode(), 1);
+
+        // Test Club Equals
+        assertNotEquals(club, null);
+        assertNotEquals(club, 3);
+        assertNotEquals(club, 'c');
+        assertEquals(club, new Suit(Suit.SuitEnum.CLUB));
+        assertEquals(club, new Suit(club.hashCode()));
+
+        // Test Club Clone
+        Suit clonedClub = club.clone();
+        assertEquals(club, clonedClub);
+        assertNotSame(club, clonedClub);
+
+        // Test Club toString
+        assertEquals(club.toString(), "" + clubSymbol);
+
+        // Test Club getSymbol
+        assertEquals(club.getSymbol(), clubSymbol);
+
+        System.out.println("Suit Club tests passed.");
+    }
+
+    @Test
+    void DiamondTest() {
+        Suit diamond = new Suit(Suit.SuitEnum.DIAMOND);
+
+        // Test Diamond HashCode
+        assertEquals(diamond.hashCode(), 2);
+
+        // Test Diamond Equals
+        assertNotEquals(diamond, null);
+        assertNotEquals(diamond, 3);
+        assertNotEquals(diamond, 'c');
+        assertEquals(diamond, new Suit(Suit.SuitEnum.DIAMOND));
+        assertEquals(diamond, new Suit(diamond.hashCode()));
+
+        // Test Diamond Clone
+        Suit clonedDiamond = diamond.clone();
+        assertEquals(diamond, clonedDiamond);
+        assertNotSame(diamond, clonedDiamond);
+
+        // Test Diamond toString
+        assertEquals(diamond.toString(), "" + diamondSymbol);
+
+        // Test Diamond getSymbol
+        assertEquals(diamond.getSymbol(), diamondSymbol);
+
+        System.out.println("Suit Diamond tests passed.");
+    }
+
+    @Test
+    void HeartTest() {
+        Suit heart = new Suit(Suit.SuitEnum.HEART);
+
+        // Test Spade HashCode
+        assertEquals(heart.hashCode(), 3);
+
+        // Test Spade Equals
+        assertNotEquals(heart, null);
+        assertNotEquals(heart, 3);
+        assertNotEquals(heart, 'c');
+        assertEquals(heart, new Suit(Suit.SuitEnum.HEART));
+        assertEquals(heart, new Suit(heart.hashCode()));
+
+        // Test Spade Clone
+        Suit clonedHeart = heart.clone();
+        assertEquals(heart, clonedHeart);
+        assertNotSame(heart, clonedHeart);
+
+        // Test Spade toString
+        assertEquals(heart.toString(), "" + heartSymbol);
+
+        // Test Spade getSymbol
+        assertEquals(heart.getSymbol(), heartSymbol);
+
+        System.out.println("Suit Heart tests passed.");
+    }
+
+    @Test
+    void ExceptionsTest() {
+        assertThrows(IllegalArgumentException.class, () -> {Suit illegalSuit = new Suit (-1);}, "Suit Index out of bound, must be bewteen 0 to 3.");
+        System.out.println("Suit Exception tests passed");
+    }
+}
